5d64ffd369eadf9b200519d4ed4db8fbafb2267e
Derrick Guerrero <derrick.guerrero@aviture.us.com>
2025-02-16 16:03:15 -0600

    feature: Enhance file hashing with sync support and customizable algorithms

--- src/getFileHash.ts
+++ src/getFileHash.ts
@@ -1,10 +1,26 @@
+import { DigestAlgorithm } from "jsr:@std/crypto/crypto";
 import { crypto } from 'jsr:@std/crypto'
 import { encodeHex } from 'jsr:@std/encoding/hex'
 
-export async function getFileHash(filePath: string) {
+export async function getFileHash(filePath: string, algorithm?: DigestAlgorithm) {
+    algorithm ??= 'SHA-256'
+
     const file = await Deno.open(filePath, { read: true })
     const stream = file.readable
-    const hashBytes = await crypto.subtle.digest('SHA-256', stream)
+    const hashBytes = await crypto.subtle.digest(algorithm, stream)
+
+    const hash = encodeHex(hashBytes)
+    return hash
+}
+
+export function getFileHashSync(filePath: string, algorithm?: DigestAlgorithm) {
+    algorithm ??= 'SHA-256'
+
+    const file = Deno.openSync(filePath, { read: true })
+    const fileInfo = Deno.statSync(filePath)
+    const buffer = new Uint8Array(fileInfo.size)
+    file.readSync(buffer)
+    const hashBytes = crypto.subtle.digestSync(algorithm, buffer)
 
     const hash = encodeHex(hashBytes)
     return hash
