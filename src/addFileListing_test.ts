015745cc36a09e9e6fa1d4b9c0b245435135ad91
Derrick Guerrero <derrick.guerrero@aviture.us.com>
2025-02-21 10:00:56 -0600

    feature: Add tests exploring sqlite file_Log functionality.

---
+++ src/addFileListing_test.ts
@@ -0,0 +1,117 @@
+import { assert } from "@std/assert/assert";
+import { DB } from "../deps.ts";
+import { FileEntry } from "./listFiles.ts";
+import { assertEquals } from "@std/assert/equals";
+
+const testDb = new DB((import.meta.dirname??'.') + '/../.test.sqlite3')
+
+Deno.test(function testAddFileListing() {
+    const testFile: FileEntry = ["parent/test.txt", 123, 456]
+
+    initAndClearFileTable(testDb)
+    addFileListing({
+        db: testDb,
+        hostname: 'test-hostname',
+        file: testFile,
+    })
+})
+
+Deno.test(function testUpdateFileListing() {
+    const testFile: FileEntry = ["parent/test.txt", 123, 456]
+    const updatedFile: FileEntry = ["parent/test.txt", 456, 789]
+
+    initAndClearFileTable(testDb)
+    addFileListing({
+        db: testDb,
+        hostname: 'test-hostname',
+        file: testFile,
+    })
+    addFileListing({
+        db: testDb,
+        hostname: 'test-hostname',
+        file: updatedFile,
+    })
+})
+
+Deno.test(function testPreviousVersionsAreArchived() {
+    const testFile: FileEntry = ["parent/test.txt", 123, 456]
+    const update1: FileEntry = ["parent/test.txt", 456, 789]
+    const update2: FileEntry = ["parent/test.txt", 789, 790]
+
+    initAndClearFileTable(testDb)
+    addFileListing({ db: testDb, hostname: 'test-hostname', file: testFile })
+    addFileListing({ db: testDb, hostname: 'test-hostname', file: update1 })
+    addFileListing({ db: testDb, hostname: 'test-hostname', file: update2 })
+
+    const rows = testDb.query<[size: number, version: number, isArchived: number]>(`
+select size, version, isArchived
+    from [files_Log]
+        `)
+    assert(rows.length === 3, `Expected 3 rows, but found ${rows.length}`)
+    assertEquals(rows, [
+        [123, 0, 1],
+        [456, 1, 1],
+        [789, 2, 0],
+    ])
+})
+
+type addFileListingParams = {
+    db: DB
+    hostname: string
+    file: FileEntry
+}
+function addFileListing({
+    db,
+    hostname,
+    file: [path, size, modifyTime],
+}: addFileListingParams) {
+    const existingAttr = db.query<[version: number, size: number, modifyTime: number]>(`
+select version, size, modifyTime
+    from (select *, row_number() over (order by version desc) [RowNum]
+        from [files_Log]
+        where hostname = ?
+            and path = ?
+        ) f
+    where f.RowNum = 1
+        `, [hostname, path])
+    assert(existingAttr.length <= 1, `Expected at most one file entry for ${hostname}:${path}, but found ${existingAttr.length} rows`)
+
+    if (existingAttr.length === 0) {
+        db.query(`
+insert into [files_Log] (hostname, path, version, size, modifyTime)
+    values (?, ?, 0, ?, ?)
+            `, [hostname, path, size, modifyTime])
+        return
+    }
+    const [[version, existingSize, existingModifyTime]] = existingAttr
+    if (existingSize === size && existingModifyTime === modifyTime) {
+        return
+    }
+
+    db.query(`
+update [files_Log] set isArchived = true
+    where hostname = ?
+        and path = ?
+        and version = ?
+        `, [hostname, path, version])
+    db.query(`
+; insert into [files_Log] (hostname, path, version, size, modifyTime)
+    values (?, ?, ?, ?, ?)
+    `, [hostname, path, version + 1, size, modifyTime])
+}
+
+function initAndClearFileTable(db: DB) {
+    db.execute(`
+        create table if not exists [files_Log] (
+            hostname text not null
+            , path text not null
+            , version bigint not null
+            , isArchived bit not null default 0
+            , size integer not null
+            , modifyTime datetime not null
+            , hash bytea null
+            , primary key (hostname, path, version)
+            )
+        ; delete from [files_Log]
+        `)
+}
