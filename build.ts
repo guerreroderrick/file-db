57d786bc1330d39d9d5befe7e535d9249cd5cc07
Derrick Guerrero <derrick.guerrero@aviture.us.com>
2025-04-06 12:46:21 -0500

    feature: Enhance build process with timing logs and support for running in another directory.

--- build.ts
+++ build.ts
@@ -4,13 +4,23 @@ if (import.meta.main) {
 }
 
 async function main() {
+    const start = Date.now()
     await runOrThrow('deno', ['check', '.'])
     await runOrThrow('deno', ['lint', '.'])
+    await runOrThrow('go', ['build'], {
+        cwd: './go',
+    })
     await runOrThrow('deno', ['task', 'test-once'])
+    console.log(`Complete all tasks after ${Date.now() - start}ms`)
 }
 
-async function runOrThrow(command: string, args?: string[]) {
+type RunOrThrowOptions = {
+    cwd?: string
+}
+async function runOrThrow(command: string, args?: string[], options?: RunOrThrowOptions) {
+    const start = Date.now()
     const cmd = new Deno.Command(command, {
+        cwd: options?.cwd,
         args,
         stdout: 'inherit',
         stderr: 'inherit',
@@ -18,6 +28,8 @@ async function runOrThrow(command: string, args?: string[]) {
     const child = cmd.spawn()
     const [status] = await Promise.all([child.status])
     if (!status.success) {
+        console.log(`${options?.cwd ?? './'} ${command} ${args?.join(' ')}: ${status.code} after ${Date.now() - start}ms`)
         throw new Error(`Command ${command} failed with code ${status.code}`)
     }
+    console.log(`${command} ${args?.join(' ')}: success after ${Date.now() - start}ms`)
 }
